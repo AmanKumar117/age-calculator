b019587037ecb76e36af6d38d74d2789555129b8
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-06-18 16:34:00 +0530

    added the age calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import DatePicker from './DatePickers';
 
 function App() {
+  const [dob, setDOB] = useState(null);
+  const [age, setAge] = useState(null);
+
+  const handleDateChange = (date) => {
+    setDOB(date);
+  };
+
+  const calculateAge = () => {
+    if (dob) {
+      const today = new Date();
+      const birthDate = new Date(dob);
+
+      let years = today.getFullYear() - birthDate.getFullYear();
+      const months = today.getMonth() - birthDate.getMonth();
+      const days = today.getDate() - birthDate.getDate();
+
+      if (months < 0 || (months === 0 && days < 0)) {
+        years--;
+      }
+
+      setAge(years);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Calculator</h1>
+      <div>
+        <h4>Select your Date of Birth:</h4>
+        <DatePicker value={dob} onChange={handleDateChange} />
+      </div>
+      <button onClick={calculateAge}>Calculate Age</button>
+      {age !== null && <p>Your age is: {age}</p>}
     </div>
   );
 }
